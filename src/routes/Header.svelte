2d50965e26958c924f239f6abd364c5a28436c0e
Nalaka Manathunga <nalaka.manathunga@izettle.com>
2023-07-05 11:37:37 +0200

    feat: adds new components

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,18 +1,28 @@
 <script>
-	import { page } from '$app/stores';
-	import github from '$lib/images/github.svg';
+	const links = [
+		{
+			label: 'About me',
+			href: '/'
+		},
+		{
+			label: 'Blog',
+			href: '/'
+		},
+		{
+			label: 'Snippets',
+			href: '/'
+		}
+	];
 </script>
 
 <header>
-	<nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8 w-full" aria-label="Global">
+	<nav
+		class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8 w-full"
+		aria-label="Global"
+	>
 		<div class="flex lg:flex-1">
-			<a href="#" class="-m-1.5 p-1.5">
-				<span class="sr-only">Your Company</span>
-				<img
-					class="h-8 w-auto"
-					src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-					alt=""
-				/>
+			<a href="#">
+				<span class="text-rose-400 text-xl font-extrabold"> N.DEV </span>
 			</a>
 		</div>
 		<div class="flex lg:hidden">
@@ -38,71 +48,38 @@
 			</button>
 		</div>
 		<div class="hidden lg:flex lg:gap-x-12">
-			<a href="#" class="text-sm font-semibold leading-6 text-gray-900">Features</a>
-			<a href="#" class="text-sm font-semibold leading-6 text-gray-900">Marketplace</a>
-			<a href="#" class="text-sm font-semibold leading-6 text-gray-900">Company</a>
+			{#each links as link}
+				<a href={link.href} class="text-sm font-semibold leading-6 text-gray-900">{link.label}</a>
+			{/each}
 		</div>
 		<div class="hidden lg:flex lg:flex-1 lg:justify-end">
 			<a href="#" class="text-sm font-semibold leading-6 text-gray-900"
-				>Log in <span aria-hidden="true">&rarr;</span></a
+				>Github <span aria-hidden="true">&rarr;</span></a
 			>
 		</div>
 	</nav>
 	<!-- Mobile menu, show/hide based on menu open state. -->
 	<div class="lg:hidden" role="dialog" aria-modal="true">
 		<!-- Background backdrop, show/hide based on slide-over state. -->
-		<div class="fixed inset-0 z-10" />
 		<div
 			class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-transparent px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
 		>
-			<div class="flex items-center justify-between">
-				<a href="#" class="-m-1.5 p-1.5">
-					<span class="sr-only">Your Company</span>
-					<img
-						class="h-8 w-auto"
-						src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-						alt=""
-					/>
-				</a>
-				<button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700">
-					<span class="sr-only">Close menu</span>
-					<svg
-						class="h-6 w-6"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke-width="1.5"
-						stroke="currentColor"
-						aria-hidden="true"
-					>
-						<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-					</svg>
-				</button>
-			</div>
 			<div class="mt-6 flow-root">
 				<div class="-my-6 divide-y divide-gray-500/10">
 					<div class="space-y-2 py-6">
-						
-						<a
-							href="#"
-							class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-							>Features</a
-						>
-						<a
-							href="#"
-							class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-							>Marketplace</a
-						>
-						<a
-							href="#"
-							class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-							>Company</a
-						>
+						{#each links as link}
+							<a
+								href={link.href}
+								class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+								>{link.label}</a
+							>
+						{/each}
 					</div>
 					<div class="py-6">
 						<a
 							href="#"
 							class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-							>Log in</a
+							>Github</a
 						>
 					</div>
 				</div>
@@ -176,15 +153,15 @@
 		position: absolute;
 		top: 0;
 		left: calc(50% - var(--size));
-		border: var(--size) solid transparent;
-		border-top: var(--size) solid var(--color-theme-1);
+		color: var(--color-theme-2);
+		border-bottom: 2px solid var(--color-theme-2);
 	}
 
 	nav a {
 		display: flex;
 		height: 100%;
 		align-items: center;
-		padding: 0 0.5rem;
+		padding: 0;
 		color: var(--color-theme-1);
 		font-weight: 400;
 		font-size: 0.8rem;
@@ -192,9 +169,11 @@
 		letter-spacing: 0.1em;
 		text-decoration: none;
 		transition: color 0.2s linear;
+		border-bottom: 2px solid transparent;
 	}
 
 	a:hover {
 		color: var(--color-theme-2);
+		border-bottom: 2px solid var(--color-theme-2);
 	}
 </style>
