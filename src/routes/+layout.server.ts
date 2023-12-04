8378a01ff136c75533a8d0b7e2e75bcce4592547
mmNalaka <nalaka.manathunga@gmail.com>
2023-12-04 20:04:57 +0100

    fix: build issue fix

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,7 +1,3 @@
 import type { LayoutServerLoad } from './$types';
 
-export const load: LayoutServerLoad = async (event) => {
-	return {
-		session: await event.locals.getSession()
-	};
-};
+export const load: LayoutServerLoad = async () => {};
